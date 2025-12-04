7e6d86fa43aa99bfa743f8e4f7804deebfb31edf
Ebenezer2077 <tobigcb547@outlook.de>
2025-12-04 16:21:59 +0100

    made setup localy

--- app/src/main/java/ToDo/app/app/Controllers/ToDoController.java
+++ app/src/main/java/ToDo/app/app/Controllers/ToDoController.java
@@ -2,8 +2,6 @@ package ToDo.app.app.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/todos")
