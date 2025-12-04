9aea083b14582523ef572e4ab7f4c65f9fdd31d5
Ebenezer2077 <149052911+Ebenezer2077@users.noreply.github.com>
2025-12-04 14:42:29 +0000

    further controller

--- app/src/main/java/ToDo/app/app/Controllers/ToDoController.java
+++ app/src/main/java/ToDo/app/app/Controllers/ToDoController.java
@@ -1,5 +1,16 @@
 package ToDo.app.app.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/todos")
 public class ToDoController {
 
+    @RequestMapping("/hello")
+    public String HelloWorld() {
+        return "Hello World";
+    }
 }
