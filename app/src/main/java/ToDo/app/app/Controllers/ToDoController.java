ac8941c9a70c915fe0589c3c6fb88c32733e18ab
Ebenezer2077 <tobigcb547@outlook.de>
2025-12-05 13:42:43 +0100

    implemented base controller

--- app/src/main/java/ToDo/app/app/Controllers/ToDoController.java
+++ app/src/main/java/ToDo/app/app/Controllers/ToDoController.java
@@ -1,14 +1,39 @@
 package ToDo.app.app.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/todos")
 public class ToDoController {
 
-    @RequestMapping("/hello")
-    public String HelloWorld() {
-        return "Hello World";
+    @GetMapping("/gettodos")
+    public String GetTodos() {
+        return "all Todos";
+    }
+
+    @GetMapping("/{id}")
+    public String GetTodoById(@PathVariable Long id) {
+        return "todo with ID: " + id;
+    }
+
+    @PostMapping("/createTodo")
+    public String CreateTodo() {
+        return "Todo Created";
+    }
+
+    @DeleteMapping("/{id}")
+    public String DeleteTodo(@PathVariable Long id) {
+        return "Todo " + id + " deleted";
+    }
+
+    @PutMapping("/{id}")
+    public String UpdateTodo(@PathVariable Long id) {
+        return "Todo " + id + "updated";
     }
 }
